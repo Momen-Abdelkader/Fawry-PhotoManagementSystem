5c823e8ada252c353c4dc0c9eb4b5004d29b0e5a
Momen Abd El-Kader <moamenabdo2004@gmail.com>
2025-07-28 18:14:57 +0300

    toString, equals, and hashCode implementation for Photo class

--- src/Photo.java
+++ src/Photo.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class Photo {
@@ -22,4 +23,33 @@ public class Photo {
     public LocalDate getDate() { return date; }
     public String getLocationName() { return locationName; }
     public Set<String> getTags() { return new HashSet<>(tags); }
+
+    @Override
+    public String toString() {
+        return String.format("Photo {id = '%s', filename = '%s', date = %s, location = '%s', tags = %s}",
+                id, photoName, date, locationName, tags);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+
+        Photo photo = (Photo) other;
+        return Objects.equals(id, photo.getId())
+                && Objects.equals(photoName, photo.photoName)
+                && Objects.equals(date, photo.date)
+                && Objects.equals(locationName, photo.locationName)
+                && Objects.equals(tags, photo.tags);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, photoName, date, locationName, tags);
+    }
 }
